4ccac6863198b4f769abc7a689148c44b196e2b3
Vostok2000 <144271978+Vostok2000@users.noreply.github.com>
2023-10-05 00:41:54 +0500

    Add files via upload

---
+++ HomeworkEmoji.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace emodji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите emoji");
+            string str = Console.ReadLine();
+            string str1 = @"^[;:]{1}\-*([\(\)\[\]]+)$";
+            bool result = Regex.Match(str, str1).Success;
+            if (result)
+            {
+                Console.WriteLine("классный смайлик!");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка, несуществующий смайлик");
+            }
+            Console.ReadKey();
+        }
+        
+    } 
+}
